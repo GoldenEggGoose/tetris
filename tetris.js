3e319b26ccb371f48e7e01713a864bc967599d20
unknown <Ridwan.Raeed.Joaddar@gmail.com>
2021-11-13 15:22:26 +0600

    added minigrid

--- tetris.js
+++ tetris.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   let grid = document.querySelector("#grid");
+  let miniGrid = document.querySelector("#miniGrid");
   let squares = document.querySelectorAll("#grid div");
+  let miniGridSquares = document.querySelectorAll("#miniGrid div");
+  const miniGridWidth = 4;
   const width = 10;
   const height = 20;
   let timeInterval = 250;
@@ -40,19 +43,55 @@ document.addEventListener("DOMContentLoaded", () => {
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
   ];
+
   let jBlock = new Block(jBlockVariations, "jBlock");
   let sBlock = new Block(sBlockVariations, "sBlock");
   let tBlock = new Block(tBlockVariations, "tBlock");
   let oBlock = new Block(oBlockVariations, "oBlock");
   let iBlock = new Block(iBlockVariations, "iBlock");
 
+  let jBlockMiniGrid = new Block(
+    [1, 2, miniGridWidth + 1, miniGridWidth * 2 + 1],
+    "jBlock"
+  );
+  let sBlockMiniGrid = new Block(
+    [
+      miniGridWidth + 1,
+      miniGridWidth + 2,
+      miniGridWidth * 2,
+      miniGridWidth * 2 + 1,
+    ],
+    "sBlock"
+  );
+  let tBlockMiniGrid = new Block(
+    [1, miniGridWidth, miniGridWidth + 1, miniGridWidth + 2],
+    "tBlock"
+  );
+  let oBlockMiniGrid = new Block(
+    [0, 1, miniGridWidth, miniGridWidth + 1],
+    "oBlock"
+  );
+  let iBlockMiniGrid = new Block(
+    [1, miniGridWidth + 1, miniGridWidth * 2 + 1, miniGridWidth * 3 + 1],
+    "iBlock"
+  );
+
   let blocks = [jBlock, sBlock, tBlock, oBlock, iBlock];
+  let miniGridBlocks = [
+    jBlockMiniGrid,
+    sBlockMiniGrid,
+    tBlockMiniGrid,
+    oBlockMiniGrid,
+    iBlockMiniGrid,
+  ];
   const getRandomBlock = () => {
     return blocks[Math.floor(Math.random() * (blocks.length - 1))];
   };
   const getRandomRotation = () => Math.floor(Math.random() * 3);
   let currentIndex = 4;
   let currentRotation = getRandomRotation();
+  let nextBlock = getRandomBlock();
+  let nextBlockIndexes = nextBlock.variations[0];
   let currentBlock = getRandomBlock();
   let currentBlockIndexes = currentBlock.variations[currentRotation];
   const drawBlock = () => {
@@ -65,13 +104,22 @@ document.addEventListener("DOMContentLoaded", () => {
       squares[currentIndex + index].classList.remove(currentBlock.className);
     });
   };
+  const eraseBlockFromTheMiniGrid = () => {
+    miniGridSquares.forEach(square => {
+      square.classList.remove(currentBlock.className);
+    });
+  };
   const spawnNewBlock = () => {
-    currentBlock = getRandomBlock();
+    currentBlock = nextBlock;
     currentIndex = 4;
+    nextBlock = getRandomBlock();
     currentRotation = getRandomRotation();
     currentBlockIndexes = currentBlock.variations[currentRotation];
     drawBlock();
   };
+  const getNextBlock = () =>{
+    return miniGridBlocks.find(block => block.className === nextBlock.className)
+  }
   const moveRight = () => {
     let thereIsASquareAgainstTheRightWall = currentBlockIndexes.some(
       (index) => (index + currentIndex) % width === width - 1
@@ -110,16 +158,22 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   const rotate = () => {
     eraseBlock();
-    currentRotation++
-    currentRotation=currentRotation%4
+    currentRotation++;
+    currentRotation = currentRotation % 4;
     currentBlockIndexes = currentBlock.variations[currentRotation];
-    let squareIsAtLeftEdge = currentBlockIndexes.some((index) => (index + currentIndex) % width === 0)
-    let squareIsAtRightEdge = currentBlockIndexes.some((index) => (index + currentIndex) % width === width - 1);
-    let someSquareIsOccupied = currentBlockIndexes.some((index) =>squares[index + currentIndex].classList.contains("occupied"))
-    if ((squareIsAtLeftEdge&&squareIsAtRightEdge)||someSquareIsOccupied){
+    let squareIsAtLeftEdge = currentBlockIndexes.some(
+      (index) => (index + currentIndex) % width === 0
+    );
+    let squareIsAtRightEdge = currentBlockIndexes.some(
+      (index) => (index + currentIndex) % width === width - 1
+    );
+    let someSquareIsOccupied = currentBlockIndexes.some((index) =>
+      squares[index + currentIndex].classList.contains("occupied")
+    );
+    if ((squareIsAtLeftEdge && squareIsAtRightEdge) || someSquareIsOccupied) {
       currentRotation--;
-      if(currentRotation<0){
-        currentRotation+=4
+      if (currentRotation < 0) {
+        currentRotation += 4;
       }
       currentBlockIndexes = currentBlock.variations[currentRotation];
     }
@@ -131,6 +185,12 @@ document.addEventListener("DOMContentLoaded", () => {
     drawBlock();
     checkAndHandleCollisions();
   };
+  let updateMiniGrid = ()=>{
+    eraseBlockFromTheMiniGrid()
+    getNextBlock().variations.forEach((index) => {
+      miniGridSquares[index].classList.add(nextBlock.className);
+    });
+  }
   document.addEventListener("keydown", (event) => {
     switch (event.code) {
       case "KeyD":
@@ -165,9 +225,11 @@ document.addEventListener("DOMContentLoaded", () => {
         squares[index + currentIndex].classList.add("occupied");
       });
       spawnNewBlock();
+      updateMiniGrid()
     }
     //if it does freeze, spawn a new block
   };
+  updateMiniGrid()
   setInterval(() => {
     moveDown();
   }, timeInterval);
